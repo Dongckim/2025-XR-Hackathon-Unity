2eb224b5b481c262b22216d5bb55fe9f6c44df1e
조희연 <146899497+joyeeon@users.noreply.github.com>
2025-07-05 23:53:05 +0900

    [feat] 타이머 UI and Debug Log

---
+++ My project/Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float time;
+    private Text text;
+    private DelayTimeMain DelayCount;
+
+    void Start()
+    {
+        DelayCount = GameObject.Find("Canvas").GetComponent<DelayTimeMain>();
+        text = this.gameObject.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (DelayCount.DelayCount == 0)
+        {
+            time += Time.deltaTime;
+            text.text = string.Format("{0:F1}", time);
+            Debug.Log("[시간 재기 UI ]Time: " + time);
+        }
+    }
+
+}
